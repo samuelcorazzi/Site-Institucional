74ac95ce3357c5306544b59f89e4b088970ea260
unknown <samuel.corazzi.prof@gmail.com>
2025-11-13 14:54:27 -0300

    cod todo comentado

--- inst.js
+++ inst.js
@@ -1,10 +1,12 @@
+/* garaante que o cod so sera executado quando a tela terminar de fazer todo o load */
 window.addEventListener("load", () => {
-  const slide = document.querySelector(".carrossel-Slide");
-  const total = slide.children.length;
+  const imagens = document.querySelector(".carrossel-imagens");
+  const total = imagens.children.length; /* conta quantas "img" tem dentro do container */
   let janela = 0;
 
+  /* setInterval seta o time do carrossel*/
   setInterval(() => {
-    janela = (janela + 1) % total;
-    slide.style.transform = `translateX(-${janela * 100}%)`;
+    janela = (janela + 1) % total; /* adc mais 1 e quando o resto for 0 ele volta do inicio */
+    imagens.style.transform = `translateX(-${janela * 100}%)`; /* faz janela * 100 o resultado negativo ele transiciona para x (esquerda) */
   }, 4000);
 });
